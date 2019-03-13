b513d92c695bb2a36b078be4035a00728a02cb96
Magana <cmagana@MBTA.COM>
2019-03-13 13:57:01 -0400

    sidebar and privateroute combined but no tab view for sidebar

--- client/src/components/authentication/PrivateRoute.js
+++ client/src/components/authentication/PrivateRoute.js
@@ -6,8 +6,11 @@
 import React, {useEffect} from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react';
 
 import {checkLoginStatus} from "../../actions";
+import Home from "../SideBar";
+
 
 class PrivateRoute extends React.Component {
 
@@ -15,6 +18,61 @@ class PrivateRoute extends React.Component {
 componentDidMount() {
     this.props.checkLoginStatus();
 }
+
+//methods for the sidebar
+    state = { visible: true };
+
+    handleHideClick = () => this.setState({ visible: false });
+    handleShowClick = () => this.setState({ visible: true });
+    handleSidebarHide = () => this.setState({ visible: true });
+
+
+
+
+
+    viewLogic = () => {
+        if (this.props.tabState === 'tools') {
+            console.log('TOOLS');
+            return this.toolsView();
+        } else if (this.props.tabState === 'intranet') {
+            console.log('intra');
+            return this.intranetView();
+        } else {
+            return null;
+        }
+    };
+
+    toolsView = () => {
+        return(
+            <div>
+                <Menu.Item as='a'>
+                    <Icon name='home' />
+                    Home
+                </Menu.Item>
+                <Menu.Item as='a'>
+                    <Icon name='gamepad' />
+                    Bids
+                </Menu.Item>
+                <Menu.Item as='a'>
+                    <Icon name='camera' />
+                    Channels
+                </Menu.Item>
+            </div>
+
+        );
+    };
+
+    intranetView = () => {
+        return(
+            <div>
+                <Menu.Item as='a'>
+                    <Icon name='home' />
+                    Nothing here yet stay tuned
+                </Menu.Item>
+
+            </div>
+        );
+    };
 
 
     render() {
@@ -25,7 +83,42 @@ componentDidMount() {
         const renderRoute = props => {
             if(rest.isSignedIn || rest.isSignedIn === null) {
                 return (
-                    <Component {...props}/>
+
+                    <div style={{minHeight: '100vh'}}>
+                        <Button.Group>
+                            <Button disabled={this.state.visible} onClick={this.handleShowClick}>
+                                Show sidebar
+                            </Button>
+                            <Button disabled={!this.state.visible} onClick={this.handleHideClick}>
+                                Hide sidebar
+                            </Button>
+                        </Button.Group>
+
+                        <Sidebar.Pushable>
+                            <Sidebar
+                                as={Menu}
+                                animation='push'
+                                icon='labeled'
+                                inverted
+                                onHide={this.handleSidebarHide}
+                                vertical
+                                visible={this.state.visible}
+                                width='wide'
+                            >
+                                <div>
+                                    {this.viewLogic()}
+                                </div>
+
+                            </Sidebar>
+
+                            <Sidebar.Pusher>
+
+
+                                <Component {...props}/>
+
+                            </Sidebar.Pusher>
+                        </Sidebar.Pushable>
+                    </div>
                 );
             }
 
