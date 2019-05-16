836b36708b5ffd48582e731e02a6f97fb46e9790
Magana <cmagana@MBTA.COM>
2019-05-16 16:07:56 -0400

    added the po no match to table but not perfect

--- client/src/components/earlyWarning/EarlyWarningTable.js
+++ client/src/components/earlyWarning/EarlyWarningTable.js
@@ -121,7 +121,7 @@ class EarlyWarningTable extends React.Component {
     );
   };
 
-  ideaList3 = () => {
+  ideaList3() {
     //filter method creates an ew array with all the elements that pass the test implemented by the provided function
 
     //some tests whether at least one element in the array passes the test implemented by the provided function. And it returns a boolean value.
@@ -129,17 +129,55 @@ class EarlyWarningTable extends React.Component {
     //map creates a new array with the results of calling a provided function on every element in the calling array
 
     //reduce executes a reducer function that is provided on each element of the array resulting a single output value.
+    let storedId='XD';
+    let check;
+    return (
+        <div style={{ overflow: "auto" }}>
+          <table className="ui single line table">
+            <thead>
+            <tr>
+              <th> Executing Department</th>
+              <th> Project_ID</th>
+              <th> Project_Name</th>
+              <th>Director</th>
+              <th>Project_Manager</th>
+            </tr>
+            </thead>
 
-    //TODO filter forest by VE/VM first
-    console.log(this.state.compositeArray);
-    let result = this.props.forestArray.filter(f => {
+            <tbody>
+            {this.props.forestArray.map(forest => {
 
-      return !this.state.compositeArray.some(c => {
-        return (f.Project_ID === c.Project_ID && (f.Executing_Department ==="Vehicle Maintenance" || f.Executing_Department === "Vehicle Engineering"));
-      });
-    });
+              //for current forestArray element names forest check if there is some Project ID that matches in the composite array
+              //if it is found return true else false
+              //also only trim by ve vm
+              const matchBool = this.state.compositeArray.some(
+                composite => composite.Project_ID === forest.Project_ID
+              );
 
-    console.log(result);
+              //filter to keep the same project Id's from showing up several times
+                if (storedId === forest.Project_ID) {
+                  check = false;
+                }
+                if (storedId !== forest.Project_ID) {
+                  check = true;
+                  storedId = forest.Project_ID;
+                }
+
+                return (
+                  <tr key={forest._id}>
+                    {!matchBool && check && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering") && <td>{forest.Executing_Department}</td>}
+                    {!matchBool && check && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering") && <td>{forest.Project_ID}</td>}
+                    {!matchBool && check && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering") && <td>{forest.Project_Name}</td>}
+                    {!matchBool && check && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering") && <td>{forest.Director}</td>}
+                    {!matchBool && check && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering") && <td>{forest.Project_Manager}</td>}
+
+                  </tr>
+                      );
+            })}
+            </tbody>
+          </table>
+        </div>
+      );
   };
 
 
@@ -176,53 +214,58 @@ class EarlyWarningTable extends React.Component {
           </thead>
 
           <tbody>
-            {this.props.forestArray.map(po => {
-              const matchingItem = this.props.earlyWarningArray.find(
-                item => po.wo_nbr === item.WO_Num && (po.Executing_Department ==="Vehicle Maintenance" || po.Executing_Department === "Vehicle Engineering")
+            {this.props.earlyWarningArray.map(early => {
+
+              const matchingItem = this.props.forestArray.find(
+                item => item.wo_nbr === early.WO_Num && (item.Executing_Department ==="Vehicle Maintenance" || item.Executing_Department === "Vehicle Engineering")
               );
 
               //let composite = [];
+
 
               if(matchingItem) {
                 //let count = 0;
                 composite[count]=matchingItem;
                 //console.log(composite);
                 this.state.compositeArray[count] = matchingItem;
-                this.state.compositeArray[count].Executing_Department = po.Executing_Department;
+
+                /*
+                this.state.compositeArray[count].Executing_Department = early.;
                 this.state.compositeArray[count].Project_ID = po.Project_ID;
                 this.state.compositeArray[count].Project_Name = po.Project_Manager;
                 this.state.compositeArray[count].Project_Manager = po.Project_Manager;
-
+                */
                 count = count + 1;
                 //console.log(this.state.compositeArray);
                 //console.log(count);
               }
 
 
+
               return (
 
-                <tr key={po._id}>
-                  {matchingItem && <td>{po.Executing_Department}</td>}
-                  {matchingItem && <td>{po.Project_ID}</td>}
-                  {matchingItem && <td>{po.Project_Name}</td>}
-                  {matchingItem && <td>{po.Director}</td>}
-                  {matchingItem && <td>{po.Project_Manager}</td>}
+                <tr key={early._id}>
+                  {matchingItem && <td>{matchingItem.Executing_Department}</td>}
+                  {matchingItem && <td>{matchingItem.Project_ID}</td>}
+                  {matchingItem && <td>{matchingItem.Project_Name}</td>}
+                  {matchingItem && <td>{matchingItem.Director}</td>}
+                  {matchingItem && <td>{matchingItem.Project_Manager}</td>}
 
-                  {matchingItem && <td>{matchingItem.WO_Num}</td>}
-                  {matchingItem && <td>{matchingItem.Req_ID}</td>}
-                  {matchingItem && <td>{this.formatDate(matchingItem.Req_Created_Date)}</td>}
-                  {matchingItem && <td>{this.formatDate(matchingItem.Req_Approval_Date)}</td>}
-                  {matchingItem && <td>{matchingItem.PO_No}</td>}
-                  {matchingItem && <td>{this.formatDate(matchingItem.PO_Date)}</td>}
-                  {matchingItem && <td>{this.formatDate(matchingItem.Date_Approved)}</td>}
-                  {matchingItem && <td>{matchingItem.Req_Descr}</td>}
-                  {matchingItem && <td>{matchingItem.Buyer}</td>}
-                  {matchingItem && <td>{matchingItem.Business_Unit}</td>}
-                  {matchingItem && <td>{matchingItem.HOLD_STATUS}</td>}
-                  {matchingItem && <td>{matchingItem.Out_to_bid}</td>}
-                  {matchingItem && <td>{matchingItem.Req_Status}</td>}
-                  {matchingItem && <td>{matchingItem.Req_Dflt_Tble_Buyer}</td>}
-                  {matchingItem && <td>{this.durationCalculation(matchingItem.Req_Approval_Date)}</td>}
+                  {matchingItem && <td>{early.WO_Num}</td>}
+                  {matchingItem && <td>{early.Req_ID}</td>}
+                  {matchingItem && <td>{this.formatDate(early.Req_Created_Date)}</td>}
+                  {matchingItem && <td>{this.formatDate(early.Req_Approval_Date)}</td>}
+                  {matchingItem && <td>{early.PO_No}</td>}
+                  {matchingItem && <td>{this.formatDate(early.PO_Date)}</td>}
+                  {matchingItem && <td>{this.formatDate(early.Date_Approved)}</td>}
+                  {matchingItem && <td>{early.Req_Descr}</td>}
+                  {matchingItem && <td>{early.Buyer}</td>}
+                  {matchingItem && <td>{early.Business_Unit}</td>}
+                  {matchingItem && <td>{early.HOLD_STATUS}</td>}
+                  {matchingItem && <td>{early.Out_to_bid}</td>}
+                  {matchingItem && <td>{early.Req_Status}</td>}
+                  {matchingItem && <td>{early.Req_Dflt_Tble_Buyer}</td>}
+                  {matchingItem && <td>{this.durationCalculation(early.Req_Approval_Date)}</td>}
 
 
 
@@ -252,7 +295,6 @@ class EarlyWarningTable extends React.Component {
     return (
         <div>
           {this.renderEarlyWarningTable()}
-          {this.ideaList2()}
           {this.ideaList3()}
         </div>
     )
