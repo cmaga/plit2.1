da176d015fe27971d12d5c222c275e9b8daf7b20
Magana <cmagana@MBTA.COM>
2019-06-07 14:55:56 -0400

    bug fixes and code clean up

--- client/src/components/earlyWarning/EarlyWarningTable.js
+++ client/src/components/earlyWarning/EarlyWarningTable.js
@@ -5,54 +5,12 @@ import _ from "lodash";
 class EarlyWarningTable extends React.Component {
     state = {compositeArray: []};
 
-
-    buildCompositeArray2 = () => {
-        let indexArray = [];
-        let matchCount = 0;
-        let count = 0;
-        //find number of matches between two arrays
-
-        for (let i = 0; i < this.props.forestArray.length; i++) {
-            console.log('outer');
-            for (let j = 0; j < this.props.earlyWarningArray.length; j++) {
-                console.log('inner');
-                if(this.props.forestArray[i].wo_nbr == this.props.earlyWarningArray[j]) {
-                    console.log('match fouond');
-                    this.state.compositeArray[count].WO_Num = this.props.earlyWarningArray[j].WO_Num;
-                    this.state.compositeArray[count].Req_ID = this.props.earlyWarningArray[j].Req_ID;
-                    this.state.compositeArray[count].Req_Created_Date = this.props.earlyWarningArray[j].Req_Created_Date;
-                    this.state.compositeArray[count].Req_Approval_Date = this.props.earlyWarningArray[j].Req_Approval_Date;
-                    this.state.compositeArray[count].PO_No = this.props.earlyWarningArray[j].PO_No;
-                    this.state.compositeArray[count].PO_Date = this.props.earlyWarningArray[j].PO_Date;
-                    this.state.compositeArray[count].Date_Approved = this.props.earlyWarningArray[j].Date_Approved;
-                    this.state.compositeArray[count].Req_Descr = this.props.earlyWarningArray[j].Req_Descr;
-                    this.state.compositeArray[count].Buyer = this.props.earlyWarningArray[j].Buyer;
-                    this.state.compositeArray[count].Business_Unit = this.props.earlyWarningArray[j].Business_Unit;
-                    this.state.compositeArray[count].HOLD_STATUS = this.props.earlyWarningArray[j].HOLD_STATUS;
-                    this.state.compositeArray[count].Out_to_bid = this.props.earlyWarningArray[j].Out_to_bid;
-                    this.state.compositeArray[count].Req_Status = this.props.earlyWarningArray[j].Req_Status;
-                    this.state.compositeArray[count].Req_Dflt_Tble_Buyer = this.props.earlyWarningArray[j].Req_Dflt_Tble_Buyer;
-                    this.state.compositeArray[count].Req_Approval_Date = this.props.earlyWarningArray[j].Req_Approval_Date;
-
-                    this.state.compositeArray[count].Project_ID = this.props.forestArray[i].Project_ID;
-                    this.state.compositeArray[count].Executing_Department = this.props.forestArray[i].Executing_Department;
-                    this.state.compositeArray[count].Project_Name = this.props.forestArray[i].Project_Name;
-                    this.state.compositeArray[count].Director = this.props.forestArray[i].Director;
-                    this.state.compositeArray[count].Project_Manager = this.props.forestArray[i].Project_Manager;
-                    count++;
-                }
-            }
-        }
-        console.log(this.state.compositeArray);
-    };
-
-
     buildCompositeArray3 = () => {
         let count = 0;
         let composite = [];
 
-        //console.log(this.props.forestArray);
-        //console.log(this.props.earlyWarningArray);
+        console.log(this.props.forestArray);
+        console.log(this.props.earlyWarningArray);
 
         this.props.forestArray.forEach(forest => {
 
@@ -61,34 +19,11 @@ class EarlyWarningTable extends React.Component {
                 item => forest.wo_nbr == item.WO_Num && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering" || forest.Executing_Department === "Safety")
             );
 
-            //console.log('earlymatch');
-            //if(earlyMatch.length > 0 && earlyMatch[0].wo_nbr === 300026)console.log(earlyMatch);
-            //this.state.compositeArray = earlyMatch;
-
             if (earlyMatch.length > 0) {
-                //composite[count] = earlyMatch;
                 //setState is not used here on purpose to many state changes caused react to throw errors. So we assign state this way without forcing a rerender.
                 earlyMatch.forEach( Match => {
-                    //change below to match
                     this.state.compositeArray[count] = Match;
-                    //console.log(earlyMatch);
-                    /*
-                    this.state.compositeArray[count].WO_Num = Match.WO_Num;
-                    this.state.compositeArray[count].Req_ID = Match.Req_ID;
-                    this.state.compositeArray[count].Req_Created_Date = Match.Req_Created_Date;
-                    this.state.compositeArray[count].Req_Approval_Date = Match.Req_Approval_Date;
-                    this.state.compositeArray[count].PO_No = Match.PO_No;
-                    this.state.compositeArray[count].PO_Date = Match.PO_Date;
-                    this.state.compositeArray[count].Date_Approved = Match.Date_Approved;
-                    this.state.compositeArray[count].Req_Descr = Match.Req_Descr;
-                    this.state.compositeArray[count].Buyer = Match.Buyer;
-                    this.state.compositeArray[count].Business_Unit = Match.Business_Unit;
-                    this.state.compositeArray[count].HOLD_STATUS = Match.HOLD_STATUS;
-                    this.state.compositeArray[count].Out_to_bid = Match.Out_to_bid;
-                    this.state.compositeArray[count].Req_Status = Match.Req_Status;
-                    this.state.compositeArray[count].Req_Dflt_Tble_Buyer = Match.Req_Dflt_Tble_Buyer;
-                    this.state.compositeArray[count].Req_Approval_Date = Match.Req_Approval_Date;
-                    */
+
                     this.state.compositeArray[count].Project_ID = forest.Project_ID;
                     this.state.compositeArray[count].Executing_Department = forest.Executing_Department;
                     this.state.compositeArray[count].Project_Name = forest.Project_Name;
@@ -103,69 +38,15 @@ class EarlyWarningTable extends React.Component {
             }
 
         });
-        //return composite = this.state.compositeArray;
     };
 
-    //contains matches between the uploaded (forest) excel sheet and the early warning data pull that is in the database by worker order number
-    buildCompositeArray = () => {
-        let count = 0;
-        let composite = [];
 
-        //console.log(this.props.forestArray);
-        //console.log(this.props.earlyWarningArray);
-
-        this.props.forestArray.forEach(forest => {
-
-            const earlyMatch = this.props.earlyWarningArray.find(
-                //intentionally comparing using == instead of === because we are comparing a string to a number
-                item => forest.wo_nbr == item.WO_Num && (forest.Executing_Department === "Vehicle Maintenance" || forest.Executing_Department === "Vehicle Engineering" || forest.Executing_Department === "Safety")
-            );
-
-            //console.log('hi');
-            //console.log(earlyMatch);
-            if (earlyMatch) {
-                composite[count] = earlyMatch;
-                //setState is not used here on purpose to many state changes caused react to throw errors. So we assign state this way without forcing a rerender.
-                this.state.compositeArray[count] = earlyMatch;
-                //console.log(earlyMatch);
-
-                this.state.compositeArray[count].WO_Num = earlyMatch.WO_Num;
-                this.state.compositeArray[count].Req_ID = earlyMatch.Req_ID;
-                this.state.compositeArray[count].Req_Created_Date = earlyMatch.Req_Created_Date;
-                this.state.compositeArray[count].Req_Approval_Date = earlyMatch.Req_Approval_Date;
-                this.state.compositeArray[count].PO_No = earlyMatch.PO_No;
-                this.state.compositeArray[count].PO_Date = earlyMatch.PO_Date;
-                this.state.compositeArray[count].Date_Approved = earlyMatch.Date_Approved;
-                this.state.compositeArray[count].Req_Descr = earlyMatch.Req_Descr;
-                this.state.compositeArray[count].Buyer = earlyMatch.Buyer;
-                this.state.compositeArray[count].Business_Unit = earlyMatch.Business_Unit;
-                this.state.compositeArray[count].HOLD_STATUS = earlyMatch.HOLD_STATUS;
-                this.state.compositeArray[count].Out_to_bid = earlyMatch.Out_to_bid;
-                this.state.compositeArray[count].Req_Status = earlyMatch.Req_Status;
-                this.state.compositeArray[count].Req_Dflt_Tble_Buyer = earlyMatch.Req_Dflt_Tble_Buyer;
-                this.state.compositeArray[count].Req_Approval_Date = earlyMatch.Req_Approval_Date;
-
-                this.state.compositeArray[count].Project_ID = forest.Project_ID;
-                this.state.compositeArray[count].Executing_Department = forest.Executing_Department;
-                this.state.compositeArray[count].Project_Name = forest.Project_Name;
-                this.state.compositeArray[count].Director = forest.Director;
-                this.state.compositeArray[count].Project_Manager = forest.Project_Manager;
-
-
-                if (earlyMatch.PO_No === null) this.state.compositeArray[count].PO_No = "";
-                count++;
-            }
-            //console.log(this.state.compositeArray);
-
-        })
-    };
     removeCopiesFromCompositeArray = () => {
-        console.log('hi');
         for( let i = 0; i < this.state.compositeArray.length; i++) {
 
             //case for array start
             if (i === 0) {
-                for (let k = i + 1; k < this.state.compositeArray.length; k++) {
+                for (let k = 1; k < this.state.compositeArray.length; k++) {
                     if (
                         _.isEqual
                         (
@@ -173,15 +54,11 @@ class EarlyWarningTable extends React.Component {
                             _.omit(this.state.compositeArray[k], ['_id'])
                         )
                     ) {
-
-                        console.log(this.state.compositeArray[i]);
-                        console.log(this.state.compositeArray[k]);
-                        console.log('space');
                         this.state.compositeArray.splice(i, 1);
                     }
                 }
-            } else if (i === this.state.compositeArray.length - 1) {
-                for (let k = 0; k < this.state.compositeArray.length - 2; k++) {
+            } else if (i === this.state.compositeArray.length - 1) { //case for end of array
+                for (let k = 0; k < this.state.compositeArray.length - 1; k++) {
                     if (
                         _.isEqual
                         (
@@ -189,17 +66,12 @@ class EarlyWarningTable extends React.Component {
                             _.omit(this.state.compositeArray[k], ['_id'])
                         )
                     ) {
-
-                        console.log(this.state.compositeArray[i]);
-                        console.log(this.state.compositeArray[k]);
-                        console.log('space');
                         this.state.compositeArray.splice(i, 1);
                     }
                 }
-            } else {
+            } else { //all other cases
                 //check if the current object is equal to the next object in the array (assumes that ordering has already happened all all copied objects are next to each other
                 for (let j = 0; j < i; j++) {
-                    //if current object is equal to any other object in the array via the loop
                     if (
                         _.isEqual
                         (
@@ -207,13 +79,9 @@ class EarlyWarningTable extends React.Component {
                             _.omit(this.state.compositeArray[j], ['_id'])
                         )
                     ) {
-
-                        console.log(this.state.compositeArray[i]);
-                        console.log(this.state.compositeArray[j]);
-                        console.log('space');
                         this.state.compositeArray.splice(i, 1);
+                        i--;
                     }
-                    //if they are remove one of them from the list of arrays.
                 }
 
                 for (let k = i + 1; k < this.state.compositeArray.length; k++) {
@@ -224,35 +92,36 @@ class EarlyWarningTable extends React.Component {
                             _.omit(this.state.compositeArray[k], ['_id'])
                         )
                     ) {
-
-                        console.log(this.state.compositeArray[i]);
-                        console.log(this.state.compositeArray[k]);
-                        console.log('space');
                         this.state.compositeArray.splice(i, 1);
+                        i--;
                     }
                 }
             }
         }
-
-
-
-
-
     };
-    formatDate = string => {
-        const options = { year: "numeric", month: "long", day: "numeric" };
-        return new Date(string).toLocaleDateString([], options);
+    formatDate = a => {
+        if (a !== null && a !== undefined) {
+            let input, monthNames, day, month, year;
+            input = a.split("T")[0].split("-");
+            day = input[2];
+            month = input[1];
+            year = input[0];
+            monthNames = "January,February,March,April,May,June,July,August,September,October,November,December";
+            monthNames = monthNames.split(",");
+            return monthNames[Number(month) - 1] + " " + day + ", " + year;
+        } return null;
     };
 
     durationCalculation = reqApprv => {
         //subtract today - req approval date
-        const today = new Date();
-        let result = fns.differenceInDays(today, reqApprv);
-
-        return result;
+        if (reqApprv !== null) {
+            const today = new Date();
+            return fns.differenceInDays(today, reqApprv);
+        } return null;
     };
 
     //this is the "compare" passed to the sort method so that it will work with objects instead of just arrays.
+    //this function may appear as not being used but it is
     compareValues(key, order='asc') {
         return function(a, b) {
             if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
@@ -264,13 +133,11 @@ class EarlyWarningTable extends React.Component {
         };
     }
 
-    //Composite array is sorted primarily for grouping of Project ID's
+    //Sorts by Vehicle engineering and then vehicle maintenance.
     sortComposite = () => {
-        this.state.compositeArray.sort(this.compareValues('Project_ID'));
+        this.state.compositeArray.sort(this.compareValues('Executing_Department'));
         //console.log(`sorted array ${this.state.compositeArray}`);
     };
-
-
 
 //idea list is the list of Po's with no requisitions: since there is no other information for them they are technically just ideas.
     ideaList = () => {
@@ -323,7 +190,7 @@ class EarlyWarningTable extends React.Component {
 
     renderEarlyWarningTableWithoutPO = () => {
         let storedId = "XD";
-        let color = "active";
+        let color = "";
         return (
             <div style={{ overflow: "auto" }}>
                 <table className="ui fixed single line compact table">
@@ -355,21 +222,18 @@ class EarlyWarningTable extends React.Component {
 
                     <tbody>
                     {this.state.compositeArray.map(matchingItem => {
-                        if (matchingItem.Project_ID !== storedId) {
+                        if (matchingItem.Project_ID !== storedId && matchingItem.PO_No === "") {
                             storedId = matchingItem.Project_ID;
                             //switch color tagsi
                             if(color==="active") {
                                 color = "";
-                                //console.log(`color switched from dark to white for ${matchingItem.Project_ID} not being equal to ${storedId}`)
                             }else {
                                 color = "active";
-                                //console.log(`color switched from white to dark for ${matchingItem.Project_ID} not being equal to ${storedId}`)
                             }
                         }
                         if (matchingItem.Project_ID === storedId) {
-                            //console.log(`${matchingItem.Project_ID} matched with ${storedId} so color did not switch`);
+                            console.log(`${matchingItem.Project_ID} matched with ${storedId} so color did not switch`);
                         }
-                        //console.log(this.state.compositeArray);
 
                         return (
 
@@ -409,7 +273,7 @@ class EarlyWarningTable extends React.Component {
 
     renderEarlyWarningTableWithPO = () => {
         let storedId = "XD";
-        let color = "active";
+        let color = "";
         return (
             <div style={{ overflow: "auto" }}>
                 <table className="ui striped fixed single line compact table">
@@ -441,24 +305,15 @@ class EarlyWarningTable extends React.Component {
 
                     <tbody>
                     {this.state.compositeArray.map(matchingItem => {
-                        //TODO this is the one that messes up the keys
                         if (matchingItem.Project_ID !== storedId) {
                             storedId = matchingItem.Project_ID;
-                            //switch color tagsi
                             if(color==="active") {
                                 color = "";
-                                //console.log(`color switched from dark to white for ${matchingItem.Project_ID} not being equal to ${storedId}`)
                             }else {
                                 color = "active";
-                                //console.log(`color switched from white to dark for ${matchingItem.Project_ID} not being equal to ${storedId}`)
                             }
                         }
-                        /*
-                        if (matchingItem.Project_ID === storedId) {
-                            console.log(`${matchingItem.Project_ID} matched with ${storedId} so color did not switch`);
-                        }
 
-                         */
                         return (
 
                             <tr key={Math.random()} className = {color}>
@@ -493,8 +348,6 @@ class EarlyWarningTable extends React.Component {
     };
 //these po's are outside of vehicle maintenance and vehicle engineering we received them via pdf and they're hard coded into this array. They were sent via a pdf.
     renderEarlyWarningTablePDF() {
-        let count = 0;
-        let composite = [];
         let hardCodedWOArray = [{wo_nbr: 301572},
             {wo_nbr: 201574},
             { wo_nbr: 301598},
@@ -586,8 +439,8 @@ class EarlyWarningTable extends React.Component {
         return (
             <div>
                 {this.buildCompositeArray3()}
-                {this.removeCopiesFromCompositeArray()};
-                {console.log(this.state.compositeArray)}
+                {this.removeCopiesFromCompositeArray()}
+                {this.sortComposite()}
 
                 <h3>Without POs</h3>
                 {this.renderEarlyWarningTableWithoutPO()}
